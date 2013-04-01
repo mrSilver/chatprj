799aa5129a0cc3481486c63d06c57da7611de6d9
mrSilver <b4pwned@gmail.com>
2013-04-01 12:30:16 +0400

    c_client.c a bit better

--- c_client.c
+++ c_client.c
@@ -1,27 +1,63 @@
+/* c_client.c */
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#define BUFF_SIZE 65507
+#define PORT_SIZE 6
+#define IP_SIZE 16
 
-void main()
+void main( int argc, char *argv[])
 {
- struct sockaddr_in server;
- int s,n;
- char b2[100];
- s=socket(AF_INET,SOCK_DGRAM,0);
- server.sin_family=AF_INET;
- server.sin_port=2000;
- server.sin_addr.s_addr=inet_addr("127.0.0.1");
- printf("\nClient ready....\n");
- n=sizeof(server);
- while (1)
- {
- printf("\nClient:");
- gets(b2);
- sendto(s,b2,sizeof(b2),0,(struct sockaddr *)&server,n);
- if(strcmp(b2,"end")==0)
-  break;
- }
+    struct sockaddr_in server;
+    int udp_socket;
+    int length;
+    char server_port_number[PORT_SIZE + 1] = {0};
+    char buffer[BUFF_SIZE];
+    unsigned int uint_spn;
+    char server_ip[IP_SIZE + 1];
+
+    int c;
+
+    while ((c = getopt (argc, argv, "p:i:")) != -1) {
+        switch (c) {
+            case 'i':
+            snprintf(server_ip,IP_SIZE,"%s",optarg);
+            break;
+        case 'p':
+            snprintf(server_port_number,PORT_SIZE,"%s",optarg);
+            uint_spn = atoi(server_port_number);
+            break;
+        case '?':
+            fprintf(stderr, "Unrecognized option!\n");
+            break;
+       }
+    }
+    printf("\nSPN = %u", uint_spn);
+
+    /*printf("\nInput server ip address:");
+    fgets(server_ip, IP_SIZE - 1, stdin);
+
+    printf("\nInput server port number:");
+    fgets(server_port_number, PORT_SIZE - 1, stdin);
+    uint_spn = atoi(server_port_number);*/
+    //printf("\nSPN = %u", uint_spn);
+
+    udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(uint_spn);
+    //printf("\nServer Port Number %d\n", ntohs(server.sin_port));
+    // shows server port number
+    inet_aton(server_ip, &server.sin_addr);
+
+    printf("\nClient ready....\n");
+    length = sizeof(server);
+    while (1) {
+        printf("\nClient:");
+        fgets(buffer, BUFF_SIZE - 1, stdin);
+        sendto(udp_socket, buffer, sizeof(buffer), 0, (struct sockaddr *) &server, length);
+    }
 }
